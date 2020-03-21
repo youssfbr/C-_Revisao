0d84f155773559b4518596a249faf29f82659641
Alisson Youssf <youssfbr@gmail.com>
2020-03-21 20:12:01 -0300

    Entrada de dados p1

---
+++ RevisaoC/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RevisaoC
+{
+    class Program
+    {
+        static void Main(string[] args)        {
+
+            string frase = Console.ReadLine();
+            string x = Console.ReadLine();
+            string y = Console.ReadLine();
+            string z = Console.ReadLine();
+            string s = Console.ReadLine();
+
+            string[] vet = s.Split(' ');
+            string a = vet[0];
+            string b = vet[1];
+            string c = vet[2];
+
+            Console.WriteLine("Você digitou:");
+            Console.WriteLine(frase);
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+            Console.WriteLine(z);
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+        }
+    }
+}
