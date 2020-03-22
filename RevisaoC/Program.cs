41ecc2de185c42b7281b420243e010ed5f66a0a2
Alisson Youssf <youssfbr@gmail.com>
2020-03-22 12:32:02 -0300

    Entrada de dados p2

--- RevisaoC/Program.cs
+++ RevisaoC/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace RevisaoC
 {
@@ -6,24 +7,25 @@ namespace RevisaoC
     {
         static void Main(string[] args)        {
 
-            string frase = Console.ReadLine();
-            string x = Console.ReadLine();
-            string y = Console.ReadLine();
-            string z = Console.ReadLine();
-            
-            string[] vet = Console.ReadLine().Split(' ');
-            string a = vet[0];
-            string b = vet[1];
-            string c = vet[2];
-
+            int n1 = int.Parse(Console.ReadLine());
+            char ch =  char.Parse(Console.ReadLine());
+            double n2 = float.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                        
+            string[] vect = Console.ReadLine().Split(" ");
+            string nome = vect[0];
+            char sexo = char.Parse(vect[1]);
+            int idade = int.Parse(vect[2]);
+            double altura = double.Parse(vect[3], CultureInfo.InvariantCulture);
+                        
             Console.WriteLine("Você digitou:");
-            Console.WriteLine(frase);
-            Console.WriteLine(x);
-            Console.WriteLine(y);
-            Console.WriteLine(z);
-            Console.WriteLine(a);
-            Console.WriteLine(b);
-            Console.WriteLine(c);
+            Console.WriteLine(n1);
+            Console.WriteLine(ch);
+            Console.WriteLine(n2.ToString("F2", CultureInfo.InvariantCulture));
+            
+            Console.WriteLine(nome);
+            Console.WriteLine(sexo);
+            Console.WriteLine(idade);
+            Console.WriteLine(altura.ToString("F2", CultureInfo.InvariantCulture));            
         }
     }
 }
